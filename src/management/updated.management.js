73e15034484ccc65ef051dc8a3bb9767f7440488
Javier Rodríguez Valentín <javrova@gmail.com>
2023-11-08 21:01:02 +0100

    Last fixes & comments

--- src/management/updated.management.js
+++ src/management/updated.management.js
@@ -1,3 +1,9 @@
+/**
+ * 
+ * @param {Object} previous -> El inmueble (objeto) antes de modificar
+ * @param {object} body -> Datos del body de la petición con la información a actualizar
+ * @returns -> objeto con los datos previos y actuales
+ */
 function updatedObject(previous, body) {
     let record = {};
     for (let element in body){
--- src/middlewares/inmuebles.middleware.js
+++ src/middlewares/inmuebles.middleware.js
@@ -1,5 +1,6 @@
 const InmuebleModel = require('../models/inmueble.model');
 
+//comprueba que el inmueble esté en el servidor y se pueda coger por el id
 const checkInmueble = async (req, res, next) => {
     try {
         const { inmuebleId } = req.params;
@@ -13,11 +14,12 @@ const checkInmueble = async (req, res, next) => {
     }
 };
 
+//comprueba que al crear un inmueble no esté ya en el servidor
 const duplicateInmueble = async (req, res, next) => {
     try {
         const inmueble = await InmuebleModel.getInmuebleByValues(req.body.floor, req.body.letter);
-        if (inmueble.length>0) {
-            return res.json({ fatal: "El inmueble ya existe" });
+        if (inmueble.length > 0) {
+            return res.json({ fatal: "El inmueble ya existe." });
         }
         next();
     } catch (error) {
@@ -25,11 +27,33 @@ const duplicateInmueble = async (req, res, next) => {
     }
 };
 
-const checkFormat = async (req,res,next) => {
+//comprueba que al actualizar un inmueble no se creen duplicados
+const noDuplicateInmueble = async (req, res, next) => {
+    try {
+        const { inmuebleId } = req.params;
+        const inmueble = await InmuebleModel.findById(inmuebleId);
+        let duplicate = [];
+        if (req.body.floor !== undefined || req.body.letter !== undefined) {
+            duplicate = await InmuebleModel.getInmuebleByValues(
+                req.body.floor !== undefined ? req.body.floor : inmueble.floor,
+                req.body.letter !== undefined ? req.body.letter : inmueble.letter
+            );
+        }
+        if (duplicate.length > 0) {
+            return res.json({ fatal: "El piso y/o letra que intenta actualizar ya está en la base de datos." });
+        }
+        next();
+    } catch (error) {
+        res.json({ fatal: error.message });
+    }
+};
+
+//Comprueba que los datos en los campos sean los correctos
+const checkFormat = async (req, res, next) => {
     try {
         const inmueble = req.body;
         let cont = 0;
-        var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
         let messageBug = `Debe solucionar los siguientes errores antes de poder guardar el inmueble`;
 
         inmueble.floor === undefined ? cont++ : !isNaN(inmueble.floor) ? cont++ : messageBug += ", Piso no es un número";
@@ -40,11 +64,11 @@ const checkFormat = async (req,res,next) => {
         inmueble.landlord === undefined ? cont++ : typeof inmueble.landlord === "string" ? cont++ : messageBug += ", Propietario no es una cadena de texto";
         inmueble.email === undefined ? cont++ : validEmail.test(inmueble.email) ? cont++ : messageBug += ", Email no es valido";
 
-        cont === 7 ? next() : res.json({fatal:  messageBug});
-        
+        cont === 7 ? next() : res.json({ fatal: messageBug });
+
     } catch (error) {
         res.json({ fatal: error.message });
     }
 }
 
-module.exports = { checkInmueble, duplicateInmueble, checkFormat }
+module.exports = { checkInmueble, duplicateInmueble, checkFormat, noDuplicateInmueble }
